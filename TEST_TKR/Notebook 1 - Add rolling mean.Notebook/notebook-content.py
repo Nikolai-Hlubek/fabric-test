6ff6e7d8cc5409fac16fda27bb9642c5b767ae4a
Nikolai Hlubek <nikolai.hlubek@gmail.com>
2025-08-13 09:16:00 +0200

    Critical fix.

--- TEST_TKR/Notebook 1 - Add rolling mean.Notebook/notebook-content.py
+++ TEST_TKR/Notebook 1 - Add rolling mean.Notebook/notebook-content.py
@@ -175,7 +175,7 @@ df_k.head(5)
 
 # CELL ********************
 
-# Export as new table
+# Export as new table. Because of reason!
 df_k.to_table("production_data_100k_running_mean", mode="overwrite")
 
 # METADATA ********************
